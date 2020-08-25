4fc00f5ca4154254a551fd47353e4d4b11d149b7
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-08-25 20:47:33 -0300

    Create component header

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './style.css';
+
+export default function Header() {
+  return (
+    <header className='container'>
+      <Link to='/'>
+        <img className='logo' src={logo} alt='logo do projeto'/>
+      </Link>
+
+      <Link className='reserva' to='/reservas'>
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>3 reservas</span>
+        </div>
+      </Link>
+
+    </header>
+  );
+}
