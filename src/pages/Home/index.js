88206be066636e6c6d047a073c89029259e13679
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-08-26 21:15:37 -0300

    Add get trips by Axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { MdFlightTakeoff } from 'react-icons/md';
+import './style.css';
 
 export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips');
+      setTrips(response.data);
+
+    }
+
+    loadApi();
+
+  }, []);
+
+
+
  return (
    <div>
      <h1>Home</h1>
