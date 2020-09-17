573fb6a94980202859cfed9eaf52a81a5e23ce52
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-09-17 19:05:18 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import Header from './components/Header';
 
+
+import history from './services/history';
+import store from './store';
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+      </Router>
+    </Provider>
   )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import api from '../../services/api';
 import { MdFlightTakeoff } from 'react-icons/md';
 import './style.css';
+import { addReserveRequest } from '../../store/modules/reserve/actions';
 
-export default function Home() {
+export default function Home( { history }) {
+  const dispatch = useDispatch();
   const [trips, setTrips] = useState([]);
+
+  const reserves = useSelector(state => state.reserve);
 
   useEffect(() => {
     async function loadApi() {
@@ -17,7 +22,11 @@ export default function Home() {
 
   }, []);
 
+  function handleAdd(id) {
+    dispatch(addReserveRequest(id));
 
+    history.push('/reservas');
+  }
 
   return (
     <div>
@@ -26,11 +35,10 @@ export default function Home() {
           <li key={trip.id}>
             <img src={trip.image} alt={trip.title} />
             <strong>{trip.title}</strong>
-            <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
-
+            <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>&nbsp;
             <button
-            type='button'
-            onClick={() => {}}
+              type='button'
+              onClick={() => handleAdd(trip.id)}
             >
               <div>
                 <MdFlightTakeoff size={16} color="#FFF" />
